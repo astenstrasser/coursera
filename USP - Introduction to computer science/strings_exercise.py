d915a54dd31ae0e246e5c96dc1312a0d2de5d946
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-03-29 16:27:19 +0100

    Added Function and Test

--- USP - Introduction to computer science/strings_exercise.py
+++ USP - Introduction to computer science/strings_exercise.py
@@ -2,6 +2,7 @@ class ListOfStrings:
 
     def __init__(self, strings_list):
         self.data = strings_list
+        self.sentence = " ".join(strings_list)
 
     def find_smaller_name(self):
         smaller = " ".join(self.data)
@@ -18,3 +19,12 @@ class ListOfStrings:
                 first = string
         return first
 
+    def upper_letters(self):
+        upper_letters = '' 
+        sentence = self.sentence
+        sentence = sentence.replace(' ','')
+        size = len(sentence)
+        for letter in sentence:
+            if ord(letter) >= 65 and ord(letter) <= 90:
+                upper_letters += letter
+        return upper_letters
--- USP - Introduction to computer science/test_strings_exercise.py
+++ USP - Introduction to computer science/test_strings_exercise.py
@@ -1,14 +1,25 @@
-import pytest 
+import pytest
 from strings_exercise import ListOfStrings
 
-class Test_order_first_string:
+
+class TestFindSmallerName:
+
+    def test_spaces_in_input(self):
+        my_input = ListOfStrings(
+            ['zé roberto', '   maria  Clara ', ' Alice  '])
+        assert my_input.find_smaller_name() == 'Alice'
+
+
+class TestOrderFirstString:
 
     def test_input_uppercase(self):
         my_input = ListOfStrings(['zé', 'maria', 'Alice'])
         assert my_input.order_first_string() == 'Alice'
-    
-class Test_find_smaller_name:
 
-    def test_spaces_in_input(self):
-        my_input = ListOfStrings(['zé roberto', '   maria  Clara ', ' Alice  '])
-        assert my_input.find_smaller_name() == 'Alice'
+
+class TestUpperLetters:
+
+    def test_input(self):
+        my_input = ListOfStrings(
+            ['Are', 'we', 'developing', 'in', 'Python', '2', '?'])
+        assert my_input.upper_letters() == 'AP'
