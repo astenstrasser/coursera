556d7eac50bd593fd908a3765a62b234cf1fb635
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-04 18:28:39 +0200

    Developing methods using recursion

--- USP - Introduction to computer science/recursion.py
+++ USP - Introduction to computer science/recursion.py
@@ -7,14 +7,39 @@ def sum_elements(list_of_elements, position = 0, sum_result = 0):
     else:
         return sum_elements(list_of_elements, position+1, sum_result)
     
-def encontra_impares(list_of_elements, position = 0, odds = []):
+def find_odds(list_of_elements, position = 0, odds = None):
+    if odds == None:
+        odds = []
     if position == (len(list_of_elements)):
+        print(odds)
         return odds
     elif list_of_elements[position] % 2 == 0:
-        print(odds)
-        return encontra_impares(list_of_elements, position+1, odds)
+        return find_odds(list_of_elements, position+1, odds)
     else:
-        print(odds)
         odds.append(list_of_elements[position])
-        return encontra_impares(list_of_elements, position+1, odds)
+        return find_odds(list_of_elements, position+1, odds)
+    
+def incomodam(times, time_check=0, my_string=''):
+    if time_check < 0:
+        return ''
+    elif time_check == times:
+        return my_string
+    else:
+        my_string += 'incomodam '
+        return incomodam(times, time_check+1, my_string)
+
+def elefantes(times, time_check=0, my_string=''):
+    if time_check < 0:
+        return ''
+    elif time_check == times:
+        return my_string
+    elif time_check == 0:
+        my_string += str(time_check+1)+ ' elefante incomoda muita gente'
+        return elefantes(times, time_check+1, my_string)   
+    else:
+        add_words = incomodam(time_check+1)
+        my_string += '\n' + str(time_check+1) + ' elefantes '+ add_words +'muito mais'
+        if time_check+1 != times:
+            my_string += '\n'+str(time_check+1) + ' elefantes incomodam muita gente'    
+        return elefantes(times, time_check+1, my_string)
     
--- USP - Introduction to computer science/test_recursion.py
+++ USP - Introduction to computer science/test_recursion.py
@@ -1,24 +1,67 @@
-from recursion import sum_elements 
+from recursion import sum_elements
 from recursion import find_odds
+from recursion import incomodam
+from recursion import elefantes
 import pytest
 
 
 class TestSumElements:
 
-    input_sum_elements = [([1,2,3,4,5,6], 21),
-                         ([1,1,1,1,1,1,1,1,1,1,1,1], 12)]
+    input_sum_elements = [([1, 2, 3, 4, 5, 6], 21),
+                          ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 12)]
 
     @pytest.mark.parametrize('list_of_elements, expected_result', input_sum_elements)
     def test_sum_elements(self, list_of_elements, expected_result):
         assert sum_elements(list_of_elements) == expected_result
 
+
 class TestFindOdds:
 
-    input_find_odds = [([1,2,3,4,5,6,7,8,9], [1,3,5,7,9])]
-                    #   ([5,5,5,5,5], [5,5,5,5,5])
-                    #   ([2,2,2,2], []),
-                    #   ([2,2,3,2], [3])]
-
-    @pytest.mark.parametrize('list_of_elements, odds', input_find_odds)
-    def test_find_odds(self, list_of_elements, odds):
+    def test_find_odds(self):
+        list_of_elements = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        odds = [1, 3, 5, 7, 9]
         assert find_odds(list_of_elements) == odds
+
+    def test_find_odds_no_odd(self):
+        list_of_elements = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+        odds = []
+        assert find_odds(list_of_elements) == odds
+
+    def test_find_odds_one_odd(self):
+        list_of_elements = [4,4,4,4,4,5,4]
+        odds = [5]
+        assert find_odds(list_of_elements) == odds
+
+
+class TestIncomodam:
+
+    test_incomodam_inputs = [(1, 'incomodam '),
+                             (0, ''),
+                             (3, 'incomodam incomodam incomodam ')]
+
+    @pytest.mark.parametrize('times, expected_output', test_incomodam_inputs)
+    def test_incomodam(self, times, expected_output):
+        assert incomodam(times) == expected_output
+
+
+class TestElefantes:
+
+    def test_elefantes(self):
+        times = 3
+        expected_output = (
+            '1 elefante incomoda muita gente\n'
+            '2 elefantes incomodam incomodam muito mais\n'
+            '2 elefantes incomodam muita gente\n'
+            '3 elefantes incomodam incomodam incomodam muito mais'
+        )   
+        assert elefantes(times) == expected_output
+
+    def test_elefantes_zero(self):
+        times = 0
+        expected_output = ''   
+        assert elefantes(times) == expected_output
+
+    def test_elefantes_one(self):
+        times = 1
+        expected_output = '1 elefante incomoda muita gente' 
+        assert elefantes(times) == expected_output
